ea89b5742da60cbdb81fb0c32614517cf732365d
Yevgen <godun.yevgen@gmail.com>
2013-03-12 16:01:04 +0200

    DONE!

--- js/common.js
+++ js/common.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-
+    $(".wtf").click(function(){
+        if ($(this).hasClass("js-active")) {
+            $(this).removeClass("js-active");
+            $(this).children(".tooltip").fadeOut();
+            $(".overlay").fadeOut();
+        }
+        else {
+            $(this).addClass("js-active")
+            $(this).children(".tooltip").fadeIn();
+            $(".overlay").fadeIn();
+        }
+    });
+    $(".overlay").click(function(){
+        $(this).fadeOut();
+        $(".tooltip").fadeOut();
+        $(".wtf").removeClass("js-active");
+    });
 });
